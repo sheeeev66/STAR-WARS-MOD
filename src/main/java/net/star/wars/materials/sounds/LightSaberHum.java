e7cf6e72fec84ecd6de40c4f1724b10fd25b64d6
OverlordsIII <OverlordsIII@users.noreply.github.com>
2020-08-15 13:28:33 -0700

    flat earther message

--- src/main/java/net/star/wars/materials/sounds/LightSaberHum.java
+++ src/main/java/net/star/wars/materials/sounds/LightSaberHum.java
@@ -16,6 +16,7 @@ public class LightSaberHum extends MovingSoundInstance {
     //but u know that
     //of courser
     //ased
+    //a
     public LightSaberHum(PlayerEntity playerIn, SoundCategory soundCategory) {
         super(StarWarsMaterials.LIGHTSABER_HUMMING, soundCategory);
         this.player = playerIn;
