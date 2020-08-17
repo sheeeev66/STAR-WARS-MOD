69cd2251be00064147b59c243baea647fb3fca42
Sheeeev66 <46237976+sheeeev66@users.noreply.github.com>
2020-08-17 15:40:24 +0300

    Update IceCrystals.java

--- src/main/java/net/star/wars/materials/block/IceCrystals.java
+++ src/main/java/net/star/wars/materials/block/IceCrystals.java
@@ -19,8 +19,6 @@ import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.IntProperty;
